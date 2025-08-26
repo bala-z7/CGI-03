f3134869c3ff918247e0fa9d31e1caa6cbdbcd23
bala-z7 <balzreylm@gmail.com>
2025-08-26 15:53:53 +0530

    Selenium Labs

---
+++ TC_010_Groups.java
@@ -0,0 +1,41 @@
+package Package1;
+ 
+import org.testng.annotations.Test;
+ 
+public class TC_010_Groups {
+
+  @Test(groups= {"smoke"})
+
+  public void test1() {
+
+	  System.out.println("This is smoketest1");
+
+  }
+
+  @Test(groups= {"regression"})
+
+  public void test2() {
+
+	  System.out.println("This is regressiontest1");
+
+  }
+
+  @Test(groups= {"regression"})
+
+  public void test3() {
+
+	  System.out.println("This is  regressiontest2");
+
+  }
+
+  @Test(groups= {"smoke"})
+
+  public void test4() {
+
+	  System.out.println("This is smoketest2");
+
+  }
+
+}
+
+ 
